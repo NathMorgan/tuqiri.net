74fd2e432d0265d6da5fad3acfe990903a5a503b
Nathan <tuqiri@tuqiri.net>
2015-01-01 14:11:45 +0000

    Added page load time

--- public/javascripts/freelancer.js
+++ public/javascripts/freelancer.js
@@ -13,6 +13,20 @@ $(function() {
     });
 })
 
+$(function(){
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-58126896-1', 'auto');
+  ga('send', 'pageview');
+})
+
+$(document).ready(function(){
+    $("#pageLoadTime").text(window.performance.timing.domContentLoadedEventEnd- window.performance.timing.navigationStart);
+})
+
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('.page-scroll a').bind('click', function(event) {
