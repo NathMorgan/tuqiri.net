8fc225d91436db27b6a724c2247cfa8c8dae49f4
Nathan <tuqiri@tuqiri.net>
2015-01-01 14:09:16 +0000

    Added favicon

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(less(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(__dirname + '/public/favicon.png'));
 
 app.use('/', routes);
 app.use('/users', users);
